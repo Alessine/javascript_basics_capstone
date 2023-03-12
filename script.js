118a784604b967524c2f6f11ba23e8490d9e0a6e
Angela Niederberger <angelaniederberger@protonmail.ch>
2023-03-12 19:02:10 +0100

    finish the js script

--- script.js
+++ script.js
@@ -1,47 +1,93 @@
-var detailsForm = document.querySelector("#destination_details_form");
+(function(){
+    "use strict";
 
-detailsForm.addEventListener("submit", handleFormSubmit);
+    const detailsForm = document.querySelector("#destination_details_form");
 
-// When someone submits the form, this function will run.
-function handleFormSubmit(event){
-    event.preventDefault();
+    detailsForm.addEventListener("submit", handleFormSubmit);
 
-    // Read out the inputs
-    var destName = event.target.elements["name"].value;
-    var destLocation = event.target.elements["location"].value;
-    var destPhoto = event.target.elements["photo"].value;
-    var destDesc = event.target.elements["description"].value;
+    // When someone submits the form, this function will run.
+    function handleFormSubmit(event){
+        event.preventDefault();
 
-    // Clear all the fields
-    for(var i=0; i<detailsForm.length; i++){
-        detailsForm.elements[i].value = "";
+        // Read out the inputs
+        const destName = event.target.elements["name"].value;
+        const destLocation = event.target.elements["location"].value;
+        const destPhoto = event.target.elements["photo"].value;
+        const destDesc = event.target.elements["description"].value;
+
+        // Clear all the fields
+        for(let i=0; i<detailsForm.length; i++){
+            detailsForm.elements[i].value = "";
+        }
+
+        // Run a function to create a new card
+        const destCard = createDestinationCard(destName, destLocation, destPhoto, destDesc);
+        const wishListContainer = document.getElementById("destination_container");
+
+        // Change the text in the heading
+        if(wishListContainer.children.length === 0){
+            document.getElementById("title").innerHTML = "My Wish List";
+        }
+
+        // Add the card
+        wishListContainer.appendChild(destCard);
+
     }
 
-    // Run a function to create a new card
+    function createDestinationCard(name, location, photoURL, description){
+        const card = document.createElement("div");
+        card.className = "card";
 
+        const img = document.createElement("img");
+        img.setAttribute("alt", name);
+        
+        const constantPhotoUrl = "images/signpost.jpg";
 
-    // Change the text in the heading
-    var wishListContainer = document.getElementById("destination_container");
+        if(photoURL.length === 0){
+            img.setAttribute("src", constantPhotoUrl);
+        }
+        else {
+            img.setAttribute("src", photoURL);
+        }
+        card.appendChild(img);
 
-    if(wishListContainer.children.length===0){
-        document.getElementById("title").innerHTML = "My Wish List";
+        const cardBody = document.createElement("div");
+        cardBody.className = "card-body";
+
+        const cardTitle = document.createElement("h3");
+        cardTitle.innerText = name;
+        cardBody.appendChild(cardTitle);
+
+        const cardSubtitle = document.createElement("h4");
+        cardSubtitle.innerText = location;
+        cardBody.appendChild(cardSubtitle);
+
+        if(description.length !== 0){
+            const cardText = document.createElement("p");
+            cardText.className = "card-text";
+            cardText.innerText = description;
+            cardBody.appendChild(cardText);
+        }
+
+        const removeButton = document.createElement("button");
+        removeButton.innerText = "Remove";
+        removeButton.addEventListener("click", removeDestination);
+        cardBody.appendChild(removeButton);
+
+        card.appendChild(cardBody);
+
+        return card
     }
 
-}
+    function removeDestination(event){
+        // Remove a card
+        const card = event.target.parentElement.parentElement;
+        card.remove();
 
-function createDestinationCard(name, location, photoURL, description){
-    var card = document.createElement(div);
-    card.className = "card";
-
-    var img = document.createElement("img");
-    img.setAttribute("alt", name);
-    
-    var constantPhotoUrl = "images/signpost.jpg";
-
-    if(photoURL.length === 0){
-        img.setAttribute("src", constantPhotoUrl);
+        // Change the text in the heading if necessary
+        const wishListContainer = document.getElementById("destination_container");
+        if(wishListContainer.children.length === 0){
+            document.getElementById("title").innerHTML = "Enter Destination Details";
+        }
     }
-    else {
-        img.setAttribute("src", photoURL);
-    }
-}
+})();
